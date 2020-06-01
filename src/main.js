87459610acf98590a11347a22b1e2a1415ba49a8
yangyan <1252430435@qq.com>
2020-06-01 17:30:25 +0800

    细节

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import routes from './config/routers';
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
-import VideoPlayer from 'vue-video-player'
+import VideoPlayer from 'vue-video-player';
 //引入 hls,视频直播(m3u8)必须引入的
 // import 'videojs-contrib-hls'
 //播放rtmp视频
@@ -15,10 +15,10 @@ const router = new VueRouter({
     mode: 'hash',
     routes  
 });
-require('video.js/dist/video-js.css')
-require('vue-video-player/src/custom-theme.css')
+require('video.js/dist/video-js.css');
+require('vue-video-player/src/custom-theme.css');
 //如果你需要自定义播放器的样式，自己新建一个css
-require('./assets/css/video.css')
+require('./assets/css/video.css');
 Vue.prototype.$http = http;
 Vue.config.productionTip = false;
 
